816e2ff3dbe463cf1e7281a6d967c6fede9b7004
Dmitriy <7ktor7@gmail.com>
2021-12-01 09:04:51 +0000

    Sanic app plus rutor parser

--- models.py
+++ models.py
@@ -7,8 +7,7 @@ async def myconn():
     # Establish a connection to an existing database named "test"
     # as a "postgres" user.
     conn = await asyncpg.connect('postgresql://bjbvafbeszwvqk:14bb331a2e746694108dc37064e958a0c209da8569dfa9b3dd36c5f36ce65af8@ec2-3-217-91-165.compute-1.amazonaws.com/d38vvdankg655o')
-    #conn = await asyncpg.connect(user='monty', password='qwerty8mM,./l',host='payk.pp.ua', port='5432', ssl=False,database='mydb77')
-    # Execute a statement to create a new table.
+    
     await conn.execute('''
         CREATE TABLE IF NOT EXISTS rutor(
             id serial PRIMARY KEY,
